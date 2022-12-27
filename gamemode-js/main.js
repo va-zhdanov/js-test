a7d4fe6630fd4b3189971e1369ed8cc01e76c292
lv-dm <rtgeorgy@gmail.com>
2022-12-27 15:48:16 +0200

    first commit

---
+++ gamemode-js/main.js
@@ -0,0 +1,9 @@
+samp.on(EVENT_PLAYER_CONNECT, (playerid) => {
+    
+    console.log("AN YDYOT CONNECTED");
+});
+
+samp.on(EVENT_PLAYER_DISCONNECT, (playerid) => {
+    
+    console.log("IDIOTIC PLAYER DISCONNECTED");
+});
